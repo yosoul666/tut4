251f715f34c0d8ac6f9f410e933fdf2f22454061
yosoul666 <981997376@qq.com>
2022-09-22 18:37:16 +0800

    qwe

---
+++ src/main/java/awe/Calc.java
@@ -0,0 +1,14 @@
+package awe;
+
+public class Calc {
+    public static  int Add(){
+        int a=0;
+        int b=0;
+        return a+b;
+    }
+    public static int Subtract(){
+        int a=2;
+        int b=2;
+        return a+b;
+    }
+}
---
+++ src/test/java/awe/CalcTest.java
@@ -0,0 +1,12 @@
+package awe;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalcTest {
+
+    @Test
+    void add() {
+    }
+}
